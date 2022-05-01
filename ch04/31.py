1e62e34afccd90035cccaecbdb3816a79d3e4d99
otsuka-pocari <akinori.otsuka.55@gmail.com>
2022-05-01 17:45:40 +0900

    ch04

--- ch04/31.py
+++ ch04/31.py
@@ -15,11 +15,16 @@ def parse_mecab(block):
         }
         res.append(lineDict)
 
+def extract_surface(block):
+    res = list(filter(lambda x: x['pos'] == '動詞', block))
+    res = [r['surface'] for r in res]
+    return res
 
 filename = 'neko.txt.mecab'
 with open(filename, mode='rt', encoding='utf-8',errors='ignore') as f:
     blocks = f.read().split('EOS\n')
 blocks = list(filter(lambda x: x != '', blocks))
 blocks = [parse_mecab(block) for block in blocks]
+ans = [extract_surface(block) for block in blocks]
 for i in range(100):
-    print(blocks[i])
+    print(ans[i])
