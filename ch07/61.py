354ea192cad9f124cd1b00b5ddcbd91a404a3086
otsuka-pocari <akinori.otsuka.55@gmail.com>
2022-05-13 14:21:28 +0900

    ch07

---
+++ ch07/61.py
@@ -0,0 +1,9 @@
+from gensim.models import KeyedVectors
+
+
+model = KeyedVectors.load_word2vec_format(
+    'GoogleNews-vectors-negative300.bin',
+    binary=True
+)
+
+print(model.similarity('United_States', 'U.S.'))
