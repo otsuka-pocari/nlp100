e484f22b3bdd8f4ea577601c0f466017aafac0ae
otsuka-pocari <akinori.otsuka.55@gmail.com>
2023-04-11 17:51:24 +0900

    change code

--- ch02/17.py
+++ ch02/17.py
@@ -6,7 +6,7 @@ col1 = [lines[i].split()[0] for i in range(len(lines))]
 col1_set_sorted = sorted(list(set(col1)))
   
 for i in range(len(col1_set_sorted)):
-    g.write(col1_set_sorted[i] + '\n')
+    g.write(col1_set_sorted[i] + "\n")
   
 f.close()
 g.close()
